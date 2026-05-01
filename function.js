3b69bfa9d0f139be6bd2294c56d83e3b90a64099
Sarvottam162 <147574477+Sarvottam162@users.noreply.github.com>
2026-05-01 20:26:24 +0530

    fat function

--- function.js
+++ function.js
@@ -19,15 +19,44 @@
 // console.log(divide(11,33));
 
 
-function add(a,b){
-    let c = a + b;
-    console.log(c);
-    return c;
-}
-console.log(add(3,5));
+// function add(a,b){
+//     let c = a + b;
+//     console.log(c);
+//     return c;
+// }
+// console.log(add(3,5));
 
-function greet(name){
-    return `hello ${name} Welcome to my page`;
+// function greet(name){
+//     return `hello ${name} Welcome to my page`;
+// }
+// console.log(greet("Sagar"));
+
+
+
+// let arr =[10,22,35,40,56]
+// for(let i = 0 ; i<arr.length; i++){
+//     if(arr[i] % 5 == 0){
+//         console.log(arr[i]);
+//     }
+// }
+
+let add = (a,b) =>{
+    return a+b;
 }
-console.log(greet("Sagar"));
+console.log(add(5,7));
+
+let sub = (a,b) =>{
+    return a-b;
+}
+console.log(sub(6,2));
+
+let mul = (a,b) =>{
+    return a*b;
+}
+console.log(mul(4,2));
+
+let div = (a,b) =>{
+    return a/b;
+}
+console.log(div(6,2));
 
